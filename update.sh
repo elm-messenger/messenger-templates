c94fa00267eb283af1ee1b89614f86f84e740745
linsyking <kxiang@umich.edu>
2025-06-10 09:53:17 +0800

    feat: update regl version
    fix: script
    
    set chmod

---
+++ update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+v=$(pnpm view elm-regl-js version)
+
+html=$(curl -s https://package.elm-lang.org/packages/linsyking/messenger-core/latest)
+core_version=$(echo "$html" | grep -oP '(?<=<title>messenger-core )[^<]+')
+
+html=$(curl -s https://package.elm-lang.org/packages/linsyking/elm-regl/latest)
+regl_version=$(echo "$html" | grep -oP '(?<=<title>elm-regl )[^<]+')
+
+sed -E -i "s/(\"linsyking\/elm-regl\":\s*\")([^\"]+)(\")/\1${regl_version}\3/" elm.json
+
+sed -E -i "s/(\"linsyking\/messenger-core\":\s*\")([^\"]+)(\")/\1${core_version}\3/" elm.json
+
+sed -E -i 's/(elm-regl-js@)[0-9]+\.[0-9]+\.[0-9]+/\1'"$v"'/' public/index.html
+sed -E -i 's/(elm-regl-js@)[0-9]+\.[0-9]+\.[0-9]+/\1'"$v"'/' public/index.min.html
+
+wget -O public/regl.js https://cdn.jsdelivr.net/npm/elm-regl-js@${v}/dist/regl.js
+wget -O public/regl.min.js https://cdn.jsdelivr.net/npm/elm-regl-js@${v}/dist/regl.min.js
