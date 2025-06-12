dad28b49c0bf5b07222c2712e8eb42a7bb52a28f
YUcxovo <junglcuxo@sjtu.edu.cn>
2025-06-13 02:53:12 +0800

    fix: raw & layered level

--- sceneproto/Raw/Level.elm
+++ sceneproto/Raw/Level.elm
@@ -9,13 +9,13 @@ module Scenes.$0.Model exposing (scene)
 
 import Lib.Base exposing (SceneMsg)
 import Lib.UserData exposing (UserData)
-import Messenger.Scene.LayeredScene exposing (LayeredSceneLevelInit)
+import Messenger.Scene.RawScene exposing (RawSceneProtoLevelInit)
 import Messenger.Scene.Scene exposing (SceneStorage)
 import SceneProtos.$1.Init exposing (InitData)
 import SceneProtos.$1.Model exposing (genScene)
 
 
-init : LayeredSceneLevelInit UserData SceneMsg InitData
+init : RawSceneProtoLevelInit UserData SceneMsg InitData
 init env msg =
     Nothing
 
